9efff6cd5ac7448d0e21df7e5f1c1c379761129f
RoastedGinger <robingurung652@gmail.com>
2018-02-12 15:41:30 +0530

    Update COMMENT.C

--- COMMENT.C
+++ COMMENT.C
@@ -3,27 +3,32 @@
 void main()
 {
   char comment[10];
-  int i,j;
+  int i,j,flag=0;
   printf("enter comment \n");
   gets(comment);
       for(i=0;i<=9;i++)
       {
-       if(comment[i] == '/' && comment[i+1] == '/')
+       if(comment[i] == '/' && comment[i+1] == '/') {
        printf("comment encountered in %d and %d index",i,i+1);
+       break;
+       }
 	 else if(comment[i] == '/' && comment[i+1] == '*')
 	      {
-		printf("comment started from %d",i);
+
 		   for(j=i+2;j<=9;j++)
 		    {
 		     if(comment[j] == '*' && comment[j+1] == '/')
 		      {
+			flag=1;
+			printf("comment started from %d",i);
 			printf("comment ended at %d ",j+1);
 		      }
 		    }
-	      }
-	      else
-	      {
-	      printf("no comments");
+		  if(flag==1)
+		  printf("its a comment");
+		  else
+		  printf("its not comment");
+
 	      break;
 	      }
        }
